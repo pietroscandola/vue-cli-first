ed39a8c3e1db699523eefc2be940f041d0e0a6d7
pietroscandola <pietro.scandolavr@gmail.com>
2022-02-07 16:10:24 +0100

    add Hero.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Title />
+    <Hero />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/Title.vue'
+import Title from './components/Title.vue'
+import Hero from './components/Hero.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Title,
+    Hero,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="jumbotron">
+      <img src="../assets/verona.jpg" alt="Verona City">
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Hero',
+}
+</script>
+
+<style>
+    .jumbotron{
+        text-align: center;
+    }
+    .jumbotron img{
+        max-width: 50%;
+    }
+</style>
